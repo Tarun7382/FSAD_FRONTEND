5685b6ebb5d89324bb0cf8307adddbb50bb0da67
Tarun7382 <2400030235@kluniversity.in>
2026-05-02 15:32:00 +0530

    nnn

--- FSAD_PROJECT/frontend1/src/services/api.js
+++ FSAD_PROJECT/frontend1/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || `${window.location.origin}/api`,
+  baseURL: import.meta.env.VITE_API_URL || "https://civic-connect-raoh.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
